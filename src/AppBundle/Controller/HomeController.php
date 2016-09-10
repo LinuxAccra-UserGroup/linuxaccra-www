3e0149cc884d2004e492f4385c3a4351d9d4e211
Laud Bentil <laud.bentil@meltwater.org>
2016-09-10 11:30:36 +0000

    add home controller

---
+++ src/AppBundle/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+
+  /**
+  * @Route("home/main")
+  */
+
+  public function mainDisplay()
+  {
+    
+  }
+}
